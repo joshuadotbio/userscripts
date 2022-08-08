da068371c393a61524c88999f2b36b21be69a377
Joshua F. Gonzales <j@joshua.bio>
2022-08-08 08:59:54 -0700

    Add script for Official Gazette of the Philippines
    Improve script for Westlaw.

---
+++ Gazette.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        Gazette
+// @description This is your new file, start writing code
+// @match       *://www.officialgazette.gov.ph/*
+// ==/UserScript==
+(() => {
+    "use strict";
+    ["html", "body"].forEach((i) => {
+        [...document.getElementsByTagName(i)].forEach((e) => {
+            e.style.cssText = 
+                "font-family: ui-serif;"
+            ;
+        });
+    });
+    ["p"].forEach((i) => {
+        [...document.getElementsByTagName(i)].forEach((e) => {
+            e.style.cssText = 
+                "font-size: 19px;" +
+                "line-height: 1.5em;" +
+                "max-width: 40em;" +
+                "-webkit-hyphens: auto;" +
+                "hyphens: auto;"
+            ;
+        });
+    });
+})();
--- Westlaw.js
+++ Westlaw.js
@@ -1,27 +1,33 @@
 // ==UserScript==
 // @name        Westlaw
-// @description This is your new file, start writing code
+// @description Make Westlaw Classic easier to read
 // @match       *://*.westlaw.com/*
 // ==/UserScript==
 (() => {
-    'use strict';
+    "use strict";
     ["body", "button", "input"].forEach((i) => {
         [...document.getElementsByTagName(i)].forEach((e) => {
-            e.style.fontFamily = "ui-sans-serif";
+            e.style.cssText = 
+                "font-family: ui-sans-serif;"
+            ;
         });
     });
     ["sup"].forEach((i) => {
         [...document.getElementsByTagName(i)].forEach((e) => {
-            e.style.verticalAlign = "baseline";
-            e.style.position = "relative";
-            e.style.top = "-0.4em";
+            e.style.cssText = 
+                "vertical-align: baseline;" +
+                "position: relative;" +
+                "top: -0.4em;"
+            ;
         });
     });
     ["co_document"].forEach((i) => {
         [...document.getElementsByClassName(i)].forEach((e) => {
-            e.style.fontFamily = "ui-serif";
-            e.style.fontSize = "19px";
-            e.style.lineHeight ="1.5em";
+            e.style.cssText = 
+                "font-family: ui-serif;" +
+                "font-size: 19px;" +
+                "line-height: 1.5em;"
+            ;
         });
     });
 })();
