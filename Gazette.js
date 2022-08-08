af84a98bd7bf25619c46f24f9574893e81982a5d
Joshua F. Gonzales <j@joshua.bio>
2022-08-08 12:02:40 -0700

    Use include instead of match

--- Gazette.js
+++ Gazette.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name        Gazette
 // @description This is your new file, start writing code
-// @match       *://www.officialgazette.gov.ph/*
+// @include       *://www.officialgazette.gov.ph/*
 // ==/UserScript==
 (() => {
     "use strict";
     ["html", "body"].forEach((i) => {
         [...document.getElementsByTagName(i)].forEach((e) => {
-            e.style.cssText = 
+            e.style.cssText =
                 "font-family: ui-serif;"
             ;
         });
     });
     ["p"].forEach((i) => {
         [...document.getElementsByTagName(i)].forEach((e) => {
-            e.style.cssText = 
+            e.style.cssText =
                 "font-size: 19px;" +
                 "line-height: 1.5em;" +
                 "max-width: 40em;" +
--- Westlaw.js
+++ Westlaw.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name        Westlaw
 // @description Make Westlaw Classic easier to read
-// @match       *://*.westlaw.com/*
+// @include       *://*.westlaw.com/*
 // ==/UserScript==
 (() => {
     "use strict";
     ["body", "button", "input"].forEach((i) => {
         [...document.getElementsByTagName(i)].forEach((e) => {
-            e.style.cssText = 
+            e.style.cssText =
                 "font-family: ui-sans-serif;"
             ;
         });
     });
     ["sup"].forEach((i) => {
         [...document.getElementsByTagName(i)].forEach((e) => {
-            e.style.cssText = 
+            e.style.cssText =
                 "vertical-align: baseline;" +
                 "position: relative;" +
                 "top: -0.4em;"
@@ -23,7 +23,7 @@
     });
     ["co_document"].forEach((i) => {
         [...document.getElementsByClassName(i)].forEach((e) => {
-            e.style.cssText = 
+            e.style.cssText =
                 "font-family: ui-serif;" +
                 "font-size: 19px;" +
                 "line-height: 1.5em;"
